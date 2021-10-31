07b84d1dd2d251feebb1e08e50954120ae972ebd
Mark Ionin <ioninmark91@gmail.com>
2021-10-31 20:10:20 +0300

    fix even_game.py

--- brain_games/even_game.py
+++ brain_games/even_game.py
@@ -5,7 +5,7 @@ from random import randint
 def even_game():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
     correct_answ_cnt = 0
     while correct_answ_cnt < 3:
@@ -17,17 +17,15 @@ def even_game():
                 print('Correct!')
                 correct_answ_cnt += 1
             else:
-                print("'{}' is wrong answer ;(."
-                " Correct answer was 'yes'.".format(answer))
-                print("Let's try again, {}!".format(name))
+                print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
+                print(f"Let's try again, {name}!")
                 return
         else:
             if answer == 'no':
                 print('Correct!')
                 correct_answ_cnt += 1
             else:
-                print("'{}' is wrong answer ;(."
-                " Correct answer was 'no'.".format(answer))
-                print("Let's try again, {}!".format(name))
+                print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
+                print(f"Let's try again, {name}!")
                 return
     print('Congratulations, {}!'.format(name))
